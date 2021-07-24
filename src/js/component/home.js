4fd110a9c786ccd2bd0703487b28d25ee951c6eb
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-07-24 15:53:24 +0000

    completed lights function

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 //include images into your bundle
-//import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = React.useState("green");
 	return (
 		<container>
 			<container className="container">
@@ -12,9 +13,29 @@ export function Home() {
 			</container>
 			<container className="container">
 				<div className="blackBox">
-					<div className="redLight"></div>
-					<div className="yellowLight"></div>
-					<div className="greenLight"></div>
+					<button
+						onClick={() => {
+							setColor("red");
+						}}
+						className={
+							"redLight" + (color === "red" ? " selected" : "")
+						}></button>
+					<button
+						onClick={() => {
+							setColor("yellow");
+						}}
+						className={
+							"yellowLight" +
+							(color === "yellow" ? " selected" : "")
+						}></button>
+					<button
+						onClick={() => {
+							setColor("green");
+						}}
+						className={
+							"greenLight" +
+							(color === "green" ? " selected" : "")
+						}></button>
 				</div>
 			</container>
 		</container>
